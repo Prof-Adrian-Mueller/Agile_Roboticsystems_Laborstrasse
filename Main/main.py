5bc1db894a86b35b6676f3429190d2a2ed056e43
David W <weyerdavid96@gmail.com>
2023-06-28 16:47:52 +0200

    Version 1.2 der Steuerzentrale (von David W)
    -> main.py wurde aktualisiert

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,13 @@
+import dobotapi
 
+# Dobot initialisieren und einer Variable zuweisen
+dobot = dobotapi.Dobot
+# Verbindung aufbauen
+dobot.ConnectDobot()
+# Lade .playback File
+playback_file = "./Testablauf.playback"
+dobot.SetPlaybackCmd(playback_file)
+# Starte das Playback
+dobot.Playback()
+# Verbindung trennen
+dobot.DisconnectDobot()
